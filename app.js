96d83b853eb58ad27f4c3827dcb15770e7712ec1
ChetanSerikar <chetanserikar87@gmail.com>
2024-11-14 03:35:19 +0530

    Added vercel routing

--- app.js
+++ app.js
@@ -29,12 +29,29 @@ connectDB();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+    }
+    next();
+  });
+
+
+
 app.use('/api/employees', employeeRoutes);
 app.use('/api/departments', departmentRoutes);
 app.get("/", (req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
   res.end("Hello World!");
 });
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: 'Something went wrong!' });
+  });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
