8f9f24522711f12cd755fd694974db3928799887
ChetanSerikar <chetanserikar87@gmail.com>
2024-11-14 03:55:48 +0530

    Fix dependency issues and update configuration

--- app.js
+++ app.js
@@ -48,10 +48,10 @@ app.get("/", (req, res) => {
   res.end("Hello World!");
 });
 
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ message: 'Something went wrong!' });
-  });
+// app.use((err, req, res, next) => {
+//     console.error(err.stack);
+//     res.status(500).json({ message: 'Something went wrong!' });
+//   });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
